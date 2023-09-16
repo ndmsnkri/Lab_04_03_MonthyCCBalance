8c6eeb82cd86263b749c0e2f96fe31f27bf8d95d
ndmsnkri <144985362+ndmsnkri@users.noreply.github.com>
2023-09-16 12:12:06 +0300

    completed version

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args)
     {
-        int initialBalance = 5000; // Starting balance
+        double initialBalance = 5000.0; // Starting balance
         double monthlyInterestRate = 0.17 ; // monthly 17% interest rate
 
         // Calculate interest after one month
         double oneMonthInterest = initialBalance * monthlyInterestRate;
 
         // Calculate interest after two months
-        double twoMonthsInterest = (initialBalance - oneMonthInterest) * monthlyInterestRate;
+        double twoMonthsInterest = (initialBalance + oneMonthInterest) * monthlyInterestRate;
 
         // Display the results
         System.out.println("Initial Balance: $" + initialBalance);
