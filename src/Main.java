1f515f51aa89802b7e364dc46f79ea73e20a8d59
ndmsnkri <144985362+ndmsnkri@users.noreply.github.com>
2023-09-16 12:05:26 +0300

    completed version

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,19 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args)
+    {
+        int initialBalance = 5000; // Starting balance
+        double monthlyInterestRate = 0.17 ; // monthly 17% interest rate
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        // Calculate interest after one month
+        double oneMonthInterest = initialBalance * monthlyInterestRate;
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        // Calculate interest after two months
+        double twoMonthsInterest = (initialBalance - oneMonthInterest) * monthlyInterestRate;
+
+        // Display the results
+        System.out.println("Initial Balance: $" + initialBalance);
+        System.out.println("Interest Due After One Month: $" + oneMonthInterest);
+        System.out.println("Interest Due After Two Months: $" + twoMonthsInterest);
+
     }
 }
